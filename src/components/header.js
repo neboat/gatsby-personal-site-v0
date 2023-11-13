ec5dbb71beea70db7a26ed11cca489df16706c2c
TB Schardl <neboat@mit.edu>
2023-11-13 07:31:05 -0500

    Fix navbar formatting

--- src/components/header.js
+++ src/components/header.js
@@ -5,16 +5,13 @@ const Header = ({ siteTitle }) => (
   <header>
     <ul className="flex border-b">
       <li className="-mb-px mr-1">
-        {/* <a className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="/">Home</a> */}
-        <Link to="/">Home</Link>
+        <Link className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" activeClassName="text-orange-700" to="/">Home</Link>
       </li>
       <li className="mr-1">
-        {/* <a className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="/cilk-highlight">Code Highlighter</a> */}
-        <Link to="/code-highlight">Code Highlighter</Link>
+        <Link className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" activeClassName="text-orange-700" to="/code-highlight">Code Highlighter</Link>
       </li>
       <li className="mr-1">
-        {/* <a className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="/about">About</a> */}
-        <Link to="/about">About</Link>
+        <Link className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" activeClassName="text-orange-700" to="/about">About</Link>
       </li>
     </ul>
   </header>
