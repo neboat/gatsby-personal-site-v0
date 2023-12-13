f68173d7699bc22ea4113f8337e1f730d9101b28
TB Schardl <neboat@mit.edu>
2023-12-13 07:02:48 -0500

    [header] Cleanup code to format navbar links consistently.

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+const navLinkClass = "bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
+const activeNavLinkClass = "text-orange-700"
+const NavLink = ({ children, ...props }) => (
+  <Link
+    {...props}
+    className={navLinkClass}
+    activeClassName={activeNavLinkClass}
+  >{children}</Link>
+)
+
 const Header = ({ siteTitle }) => (
   <header>
     <ul className="flex border-b">
       <li className="-mb-px mr-1">
-        <Link className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" activeClassName="text-orange-700" to="/">Home</Link>
+        <NavLink to="/">Home</NavLink>
       </li>
       <li className="mr-1">
-        <Link className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" activeClassName="text-orange-700" to="/code-highlight">Code Highlighter</Link>
+        <NavLink to="/code-highlight">Code Highlighter</NavLink>
       </li>
       <li className="mr-1">
-        <Link className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" activeClassName="text-orange-700" to="/about">About</Link>
+        <NavLink to="/about">About</NavLink>
       </li>
     </ul>
   </header>
