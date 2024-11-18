516e645c836f9668ca51428d51c5109232c75435
TB Schardl <neboat@mit.edu>
2024-11-17 19:19:26 -0500

    [code-highlight] Override prose width on code-highlight page.

--- src/pages/code-highlight/index.js
+++ src/pages/code-highlight/index.js
@@ -145,7 +145,7 @@ const CodeHighlightPage = () => {
     return (
         <Layout pageTitle="Code Highlighter">
             <div className="w-full mb-4">
-                <article className="prose prose-zinc dark:prose-invert prose-code:before:hidden prose-code:after:hidden prose-inline-code:bg-amber-50 dark:prose-inline-code:bg-amber-950 max-w-full">
+                <article className="prose prose-zinc dark:prose-invert prose-code:before:hidden prose-code:after:hidden prose-inline-code:bg-amber-50 dark:prose-inline-code:bg-amber-950 !max-w-full">
                     <h1>Code Highlighter</h1>
                     <CodeInputOutput />
                     <Description />
